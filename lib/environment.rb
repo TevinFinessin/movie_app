28e425c61d81fac2706cf4fa397d7647d2571ce3
Tevin Sherman <tevinsherman@yahoo.com>
2019-10-05 22:18:15 -0400

    rename file

---
+++ lib/environment.rb
@@ -0,0 +1,6 @@
+require "movie_app/version"
+
+# module MovieApp
+ # class Error < StandardError; end
+  # Your code goes here...
+# end
--- lib/movie_app.rb
+++
@@ -1,6 +0,0 @@
-require "movie_app/version"
-
-module MovieApp
-  class Error < StandardError; end
-  # Your code goes here...
-end
