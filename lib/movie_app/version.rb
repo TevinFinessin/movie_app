a70c95205d55ae9bd28619c98178dc26beead36b
Tevin Sherman <tevinsherman@yahoo.com>
2019-10-05 22:17:23 -0400

    require environment

---
+++ lib/movie_app/version.rb
@@ -0,0 +1,3 @@
+module MovieApp
+  VERSION = "0.1.0"
+end
---
+++ spec/movie_app_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe MovieApp do
+  it "has a version number" do
+    expect(MovieApp::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
